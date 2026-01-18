c9890a3d4da77d7ead908eeed933df3db75bc81c
goBrainstorm <luck.janning@gmail.com>
2026-01-18 19:03:23 +0200

    Refactor FileHandler class to improve output file management
    This update modifies the FileHandler class to enhance the handling of output files and directories. Changes include the introduction of constants for output file naming and structure, ensuring a consistent file path format. The create_output_file method now directly references the new constants, and the functionality to create individual output files is marked for future implementation.

--- src/file_handling.py
+++ src/file_handling.py
@@ -6,8 +6,10 @@ import time
 class FileHandler:
     DEFAULT_MODEL_PATH = "models/whisper"
     
-    DEFAULT_OUTPUT_FOLDER = "output"
-    DEFAULT_OUTPUT_FILE = DEFAULT_OUTPUT_FOLDER + "/output.json"
+    DEFAULT_OUTPUT_FOLDER = "output/"
+    DEFAULT_OUTPUT_FILE_NAME = "output"
+    DEFAULT_FILE_EXTENSION = ".json"
+    DEFAULT_OUTPUT_FILE = DEFAULT_OUTPUT_FILE_NAME + DEFAULT_FILE_EXTENSION
     DEFAULT_STRUCTURE = {
         "date": "YYYY-MM-DD",
         "time": "HH:MM:SS",
@@ -15,34 +17,23 @@ class FileHandler:
         "transcript": "",
     }
 
-    DEFAULT_FILE_EXTENSION = ".json"
-
-    files_in_output_folder = []
-
     def __init__(self, output_folder: str = DEFAULT_OUTPUT_FOLDER, output_file: str = DEFAULT_OUTPUT_FILE):
-        self.output_folder = output_folder
-        self.output_file = output_file
+        pass #TODO: add functionallity to create new individual output files
 
     def create_output_folder(self) -> None:
-        if not os.path.exists(self.output_folder):
-            os.makedirs(self.output_folder)
+        if not os.path.exists(self.DEFAULT_OUTPUT_FOLDER):
+            os.makedirs(self.DEFAULT_OUTPUT_FOLDER)
 
     def create_output_file(self, ignore_existing: bool = False) -> None: #TODO: add functionallity to create new individual output files
         """Create a new output file. If the file already exists, the file will be overwritten."""
         self.create_output_folder()
-        if not os.path.exists(self.output_file) or ignore_existing:
-            if ignore_existing:
-                self.output_file = self.output_file + "_" + time.strftime("%Y%m%d_%H%M%S") + self.DEFAULT_FILE_EXTENSION
-            with open(self.output_file, "w") as f:
+        if not os.path.exists(self.DEFAULT_OUTPUT_FOLDER + self.DEFAULT_OUTPUT_FILE) or ignore_existing:
+            with open(self.DEFAULT_OUTPUT_FOLDER + self.DEFAULT_OUTPUT_FILE, "w") as f:
                 json.dump(self.DEFAULT_STRUCTURE, f)
-            self.files_in_output_folder.append(self.output_file)
 
     def write_to_output_file(self, data: dict) -> None:
-        with open(self.output_file, "w") as f:
+        with open(self.DEFAULT_OUTPUT_FOLDER + self.DEFAULT_OUTPUT_FILE, "w") as f:
             json.dump(data, f)
 
-    def get_output_path(self) -> str:
-        return os.path.join(self.output_folder, self.output_file)
-
-    def get_available_models(self, path: str = DEFAULT_MODEL_PATH):
-        return [f for f in os.listdir(path) if f.endswith('.bin')]
+    def get_available_models(self) -> list:
+        return [f for f in os.listdir(self.DEFAULT_MODEL_PATH) if f.endswith('.bin')]
